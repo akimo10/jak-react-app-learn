51c9a6e8d46d2e9bf5698073c4e2169d72f2db6c
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-19 19:52:42 +0900

    ログインメッセージ機能を実装

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,9 +2,11 @@ import axios from "axios";
 import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
   const [loading, setLoading] = useState(false);
   const login = useCallback(
     (id: string) => {
@@ -13,12 +15,15 @@ export const useAuth = () => {
         .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({ title: "ログインしました", status: "success" });
             history.push("/home");
           } else {
-            alert("ユーザーがみつかりません");
+            showMessage({ title: "ユーザーがみつかりません", status: "error" });
           }
         })
-        .catch(() => alert("ログインできませんでした"))
+        .catch(() =>
+          showMessage({ title: "ログインできませんでした", status: "error" })
+        )
         .finally(() => {
           setLoading(false);
         });
